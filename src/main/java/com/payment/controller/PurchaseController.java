f3c264bdc74914e94494c7593f7a58748178bda7
zhenyajzz <yauheni.hancharou@inbox.ru>
2026-03-18 03:47:58 +0300

    [PL-6] - refactoring controller

--- src/main/java/com/payment/controller/PurchaseController.java
+++ src/main/java/com/payment/controller/PurchaseController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -21,10 +24,25 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
 
     @PostMapping
-    public ResponseEntity<PurchaseResponse> create(@RequestBody @Valid PurchaseRequest purchaseRequest) {
+    public ResponseEntity<Map<String, Integer>> purchase(@RequestBody @Valid PurchaseRequest purchaseRequest) {
+        purchaseService.purchaseProduct(purchaseRequest);
+        return ResponseEntity.ok(Collections.singletonMap("status", 200));
+    }
+
+    @PostMapping
+    public ResponseEntity<PurchaseResponse> purchaseDetails(@RequestBody @Valid PurchaseRequest purchaseRequest) {
         log.info("Received purchase request: {}", purchaseRequest);
         PurchaseResponse response = purchaseService.purchaseProduct(purchaseRequest);
         log.info("Purchase completed: {}", response);
         return ResponseEntity.ok(response);
     }
+    //*
+    // Развернутый ответ для метода purchaseDetails
+    //    "couponCode": "P10",
+    //    "paymentProcessor": "stripe",
+    //    "paymentSuccess": true,
+    //    "productName": "Iphone",
+    //    "taxNumber": "DE123456789",
+    //    "totalPrice": 107.1
+    // */
 }
